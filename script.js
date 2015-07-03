23007bf8c083cd0c57b305517a75c3c16866582c
Vaden Masrani <vadmas@gmail.com>
2015-07-03 15:27:46 -0700

    Added tooltip, changed y axis to ordinal, separated colours into lanes, added highlight on hover

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ vis = {}
 var margin, width, height;
 var chart,svg;
 var x,y;
+var datePrinter = d3.time.format("%x");
 //==============end of global declaration===========
 
 
@@ -16,8 +17,8 @@ vis.init = function(params) {
    if (!params) {params = {}}
 
    chart = d3.select(params.chart||"#chart"); // placeholder div for svg
-   margin = {top: 40, right: 40, bottom: 40, left: 40};
-   width  = ( params.width  || 960 ) - margin.left - margin.right;
+   margin = {top: 40, right: 40, bottom: 40, left: 60};
+   width  = ( params.width  || 1060 ) - margin.left - margin.right;
    height = ( params.height || 500 ) - margin.top - margin.bottom;
 
    chart.selectAll("svg")
@@ -29,8 +30,8 @@ vis.init = function(params) {
    // to the svg. this doesn't create new svg elements. 	  
 
    svg = d3.select("svg").attr({
-		width: function(d) {return d.width  + margin.left + margin.right},
-		height:function(d) {return d.height + margin.top + margin.bottom}
+  		width: function(d) {return d.width  + margin.left + margin.right},
+  		height:function(d) {return d.height + margin.top + margin.bottom}
 		})
    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -49,19 +50,46 @@ vis.init = function(params) {
         .append("rect")
           .attr("width", width)
           .attr("height", height);
-  
+
+  // tooltip
+  tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return datePrinter(d.date); });
+  tip.direction('n');
+  tip.offset([-5, 0])
+  svg.call(tip)
+
   // -------------------------
   // Set up scales + axis
   // -------------------------
-  var xmax = d3.max(params.data,function(d){return d.xcoord})*1.05;
-  var ymax = d3.max(params.data,function(d){return d.ycoord})*1.05;
 
-  x = d3.scale.linear()
-    .range([0, width]).domain([0,xmax]);
+  //---------------
+  // Make date objects
+  params.data.forEach(function(d){
+    d.date = new Date(d.date);
+  })
+  params.redline = new Date(params.redline)
+  //---------------
 
-  // Offset by 3 pixels to preserve black border
-  y = d3.scale.linear()
-    .range([height-3, 0]).domain([0,ymax]);
+  var xmin = d3.min(params.data,function(d){return d.date});
+  var xmax = d3.max(params.data,function(d){return d.date});
+
+  x = d3.time.scale().domain([d3.time.day.offset(xmin, -2), d3.time.day.offset(xmax, 2)]).range([0, width]);
+  
+  // Ordinal scale, hardcoded for now
+  y = d3.scale.ordinal()
+  .domain(["offset",
+           "1a",
+           "1b",
+           "2a",
+           "2b",
+           "3a",
+           "3b",
+           "4a",
+           "4b",
+           "5a",
+           "5b",
+           "6a",
+           "6b"]).rangeBands([0,height]);
+
 
   var xAxis = d3.svg.axis()
     .scale(x)
@@ -71,9 +99,20 @@ vis.init = function(params) {
   var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left")
-    .ticks(6)
-    .tickSize(-width+1);
-    // .outerTickSize(0);
+    .tickValues(y.domain().filter(function(d, i) { return !(i % 2); })) //Hide every second tick
+    .tickSize(-width+1).
+    tickFormat(function (d) {
+      var mapper = {
+       "offset":"",
+       "1b": "111657",
+       "2b": "25910",
+       "3b": "147183",
+       "4b": "98182",
+       "5b": "4866",
+       "6b": "23532",
+      }
+    return mapper[d]
+  });
 
   svg.append("g")
     .attr("class", "x axis")
@@ -100,26 +139,53 @@ vis.init = function(params) {
   var shapes = dataPoints.selectAll(".datapoints")
    	.data(params.data).enter();
    
+   // Append circles
    shapes.append("path")
-   	.filter(function(d) { return d.type == 0 })
-   	  .attr("class", "shapes circle")
-      .attr("transform", function(d) { return "translate(" + x(d.xcoord) + "," + y(d.ycoord) + ")"; })
-      .attr("d", d3.svg.symbol().type("circle"))
-      .style("fill", "#00695C");
+      .attr("class", "shapes")
+      .style("stroke-width", 1.2)
+      .style("stroke", "black")
+      .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(d.ycoord) + ")"; })
+      .each(function(d){
+        var path = d3.select(this);
 
-   shapes.append("path")
-   	.filter(function(d) { return d.type == 1 })
-   	  .attr("class", "shapes square")
-      .attr("transform", function(d) { return "translate(" + x(d.xcoord) + "," + y(d.ycoord) + ")"; })
-      .attr("d", d3.svg.symbol().type("square"))
-      .style("fill", "#0277BD");
+        // "a" type get green colour "b" type get blue colour
+        var colour = "#00695C";
+        if (d.ycoord.indexOf("b") != -1) colour = "#0277BD";
 
-   shapes.append("path")
-   	.filter(function(d) { return d.type == 2 })
-   	  .attr("class", "shapes triangle")
-      .attr("transform", function(d) { return "translate(" + x(d.xcoord) + "," + y(d.ycoord) + ")"; })
-      .attr("d", d3.svg.symbol().type("triangle-up"))
-      .style("fill", "#651FFF");
+        // Save so highlight can return to default colour
+        d.colour = colour
+
+        // eg Commit
+        if (d.type == 0) {
+          path.attr("d", d3.svg.symbol().type("circle").size(128)).style("fill", d.colour);
+          path.classed("circle")
+        }
+        // eg Pull
+        if (d.type == 1) {
+          path.attr("d", d3.svg.symbol().type("square").size(128)).style("fill", d.colour);
+          path.classed("square")
+        }
+      })
+      .on("mouseover", function(d) { 
+          tip.show(d);
+          d3.select(this).style("stroke", "#F44336"); 
+          d3.select(this).style("stroke-width", 2.5); 
+        })
+      .on("mouseout",  function(d) { 
+          tip.hide(d);
+          d3.select(this).style("stroke", "black"); 
+          d3.select(this).style("stroke-width", 1); 
+        });
+      
+      
+
+  // // Append triangles
+  //  shapes.append("path")
+  //  	.filter(function(d) { return d.type == 2 })
+  //  	  .attr("class", "shapes triangle")
+  //     .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(d.ycoord) + ")"; })
+  //     .attr("d", d3.svg.symbol().type("triangle-up"))
+  //     .style("fill", "#651FFF");
   // -------------------------
 
 
@@ -132,7 +198,7 @@ vis.init = function(params) {
       .y(function(d) { return d.y;})
       .interpolate("linear");
 
-    var lineData = [[{"x": 5435,"y": 0},{"x": 5435,"y":height}]]
+    var lineData = [[{"x": params.redline,"y": 0},{"x": params.redline,"y":height}]]
     
     dataPoints.selectAll(".line")
       .data(lineData)
@@ -154,13 +220,7 @@ vis.init = function(params) {
       .scaleExtent([1, 100])
       .on("zoom", zoomed);
 
-  // Make translucent zoom area so user can zoom on entire chart
-  var zoomarea = svg.append("rect")
-        .attr("id"    , "zoomspace")
-        .attr("style" , "opacity:0")
-        .attr("width" , width)
-        .attr("height", height)
-        .call(zoom);
+  svg.call(zoom)
 
   function zoomed() {
 
@@ -175,7 +235,7 @@ vis.init = function(params) {
 	  svg.select(".x.axis").call(xAxis);
 	  // Update points
 	  dataPoints.selectAll(".shapes")
-       .attr("transform", function(d) { return "translate(" + x(d.xcoord) + "," + y(d.ycoord) + ")"; });    
+       .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(d.ycoord) + ")"; });    
 
     dataPoints.selectAll('.line').attr("d", line)
 	}       
